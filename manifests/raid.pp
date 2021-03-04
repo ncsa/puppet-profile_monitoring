7fc2ec7a89bdc4d3e81b6e8c8751608e319084f6
Bill Glick <bglick@illinois.edu>
2021-03-04 12:48:48 -0600

    first commit

---
+++ manifests/raid.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile_monitoring::raid
+class profile_monitoring::raid {
+}
---
+++ manifests/telegraf.pp
@@ -0,0 +1,60 @@
+# @summary Setup telegraf on a node
+#
+# @param outputs
+#   Define output types and parameters for each.
+#   Outputs defined here are all named, therefore allow parameter merging
+#   across multiple layers of hiera
+#
+# @param required_pkgs
+#   OS packages that should be installed for this service to operate
+#   Hash format is:
+#   ```
+#   pkg_name: {pkg_options}
+#   ```
+#   where `pkg_options` are valid Puppet package attributes.
+#   
+# @example
+#   include profile_monitoring::telegraf
+#
+class profile_monitoring::telegraf (
+  Hash $outputs,
+  Hash $required_pkgs,
+) {
+
+  include ::telegraf
+
+  # Ensure required packages
+  ensure_packages( $required_pkgs, {'ensure' => 'installed' } )
+
+  # Install outputs
+  $outputs.each | $plugin_type, $entry | {
+    $entry.each | $entry_name, $options | {
+      telegraf::output { $entry_name :
+        plugin_type => $plugin_type,
+        options     => [ $options ],
+      }
+    }
+  }
+
+  # Place udev rule for ipmi commands on nodes running telegraf
+  $udevrules_ipmi = 'KERNEL=="ipmi*", MODE="660", GROUP="telegraf"'
+  file { '/lib/udev/rules.d/52-telegraf-ipmi.rules':
+    ensure  => 'present',
+    mode    => '0644',
+    content => $udevrules_ipmi,
+    notify  => Exec[ 'udevadm4telegraf' ],
+  }
+
+  exec { 'udevadm4telegraf':
+    command     => '/sbin/udevadm control --reload-rules && /sbin/udevadm trigger',
+    refreshonly => true,
+  }
+
+  # Ensure telegraf can read puppet stats
+  file { '/opt/puppetlabs/puppet/cache':
+    ensure => 'directory',
+    mode   => '0755',
+  }
+
+}
+
